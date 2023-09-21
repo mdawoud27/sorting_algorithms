07357e4777df41abd3655f036f131305ab548c28
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2023-09-21 17:38:16 +0000

    Update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t flag = 1, i, j;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
